d22fc7252b3cd9dd04352c9b1f340f9b4168f43d
BARIS BAHADIR SAVUR <barisbahadir@live.com>
2024-12-02 23:32:44 +0300

    Project files add

---
+++ src/main/java/com/bahadir/bahadir_pos_api/BahadirPosApiApplication.java
@@ -0,0 +1,13 @@
+package com.bahadir.bahadir_pos_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BahadirPosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BahadirPosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bahadir/bahadir_pos_api/BahadirPosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bahadir.bahadir_pos_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BahadirPosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
